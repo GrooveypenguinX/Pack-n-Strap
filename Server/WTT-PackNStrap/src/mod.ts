8c2f105cb36b728918ea84ee667c35166f746b3a
GrooveypenguinX <jakechase2110@gmail.com>
2024-04-18 18:51:51 -0400

    added new config options, changed loyal level for pointman to 2

--- Server/WTT-PackNStrap/src/mod.ts
+++ Server/WTT-PackNStrap/src/mod.ts
@@ -8,6 +8,9 @@ import { IPreAkiLoadMod } from "@spt-aki/models/external/IPreAkiLoadMod";
 import { LogTextColor } from "@spt-aki/models/spt/logging/LogTextColor";
 import type { GameController } from "@spt-aki/controllers/GameController";
 import type { IEmptyRequestData } from "@spt-aki/models/eft/common/IEmptyRequestData";
+import { InRaidHelper } from "@spt-aki/helpers/InRaidHelper";
+import { IPmcData } from "@spt-aki/models/eft/common/IPmcData";
+import { Item } from "@spt-aki/models/eft/common/tables/IItem";
 import * as config from "../config/config.json";
 
 // WTT imports
@@ -28,10 +31,34 @@ class PackNStrap
 
     debug = false;
 
+    oldSmallCaseIds = [
+        "container_smallscavcase",
+        "container_toolpouch",
+        "container_smalldocscase",
+        "container_medpouch",
+        "container_ammopouch",
+        "container_magpouch",
+        "container_lunchbox",
+        "container_keyring"
+    ];
+
+    newIdMap = {
+        container_smallscavcase: "0c22fc270f59b28c064e1232",
+        container_toolpouch: "9543bbe8083934dc3b1b1330",
+        container_smalldocscase: "c29f11b2e63a089916739c96",
+        container_medpouch: "12403f74773f49be6a2d84b7",
+        container_ammopouch: "ae9e418fd5d4c4eec4a0e6ea",
+        container_magpouch: "440de5d056825485a0cf3a19",
+        container_lunchbox: "6925918065a41e6b1e02a7d7",
+        container_keyring: "2eabd4da4ab194eb168e72d3"
+    };
     public preAkiLoad(container: DependencyContainer): void {
         this.Instance.preAkiLoad(container, this.modName);
         this.Instance.debug = this.debug;
         this.fixStupidMongoIds();
+        if (!config.loseArmbandOnDeath) {
+            this.keepItemsInArmbandAfterDeath();
+        }
         this.PackNStrapItemService.preAkiLoad(this.Instance);
 
     }
@@ -46,6 +73,21 @@ class PackNStrap
         if (config.loseArmbandOnDeath) {
             const dblostondeathConfig = this.Instance.configServer.getConfig<ILostOnDeathConfig>(ConfigTypes.LOST_ON_DEATH)
             dblostondeathConfig.equipment.ArmBand = true;
+        }
+        if (config.addCasesToSecureContainer) {
+            for (const caseId of Object.values(this.newIdMap)) {
+                for (const item of Object.values(this.Instance.database.templates.items)) {
+                    if (item._parent === "5448bf274bdc2dfc2f8b456a") {
+                        const filters = item._props.Grids[0]._props.filters[0];
+                        if (filters.Filter === undefined) {
+                            filters.Filter = [caseId];
+                        }
+                        else if (filters) {
+                            filters.Filter.push(caseId);
+                        }
+                    }
+                }
+            }
         }
     }
 
@@ -68,30 +110,31 @@ class PackNStrap
 
     }
 
+    public keepItemsInArmbandAfterDeath(): void {
+        this.Instance.container.afterResolution("InRaidHelper", (_, result: any) => {
+            const originalisItemKeptAfterDeath = result.isItemKeptAfterDeath;
+    
+            result.isItemKeptAfterDeath = (pmcData: IPmcData, itemToCheck: Item): boolean => {
+                // Extract the _id of the item with slotId "ArmBand"
+                const armBandItem = pmcData.Inventory.items.find(item => item.slotId === "ArmBand");
+                const armBandItemId = armBandItem?._id;
+    
+                // Check if itemToCheck has the extracted _id as its parentId
+                if (armBandItemId && itemToCheck.parentId === armBandItemId) {
+                    return true; // Keep the item after death
+                }
+                // Otherwise, defer to the original method for other items
+                return originalisItemKeptAfterDeath.call(result, pmcData, itemToCheck);
+            };
+        });
+    }
+    
+
     // Handle updating the user profile between versions:
     // - Update the container IDs to the new MongoID format
     // - Look for any key cases in the user's inventory, and properly update the child key locations if we've moved them
     public fixProfile(sessionId: string) {
-        const oldSmallCaseIds = [
-            "container_smallscavcase",
-            "container_toolpouch",
-            "container_smalldocscase",
-            "container_medpouch",
-            "container_ammopouch",
-            "container_magpouch",
-            "container_lunchbox",
-            "container_keyring"
-        ];
-        const newIdMap = {
-            container_smallscavcase: "0c22fc270f59b28c064e1232",
-            container_toolpouch: "9543bbe8083934dc3b1b1330",
-            container_smalldocscase: "c29f11b2e63a089916739c96",
-            container_medpouch: "12403f74773f49be6a2d84b7",
-            container_ammopouch: "ae9e418fd5d4c4eec4a0e6ea",
-            container_magpouch: "440de5d056825485a0cf3a19",
-            container_lunchbox: "6925918065a41e6b1e02a7d7",
-            container_keyring: "2eabd4da4ab194eb168e72d3"
-        };
+
 
         const dbItems = this.Instance.database.templates.items;
         const pmcProfile = this.Instance.profileHelper.getFullProfile(sessionId)?.characters?.pmc;
@@ -101,8 +144,8 @@ class PackNStrap
 
         // Update the container IDs to the new MongoID format
         for (const item of pmcProfile.Inventory.items) {
-            if (newIdMap[item._tpl]) {
-                item._tpl = newIdMap[item._tpl];
+            if (this.newIdMap[item._tpl]) {
+                item._tpl = this.newIdMap[item._tpl];
             }
         }
 
@@ -110,9 +153,9 @@ class PackNStrap
         const pmcInventory = structuredClone(pmcProfile.Inventory.items);
 
         // Look for any key cases in the user's inventory, and properly update the child key locations if we've moved them
-        for (const oldCaseId of oldSmallCaseIds) {
-            if (newIdMap[oldCaseId]) {
-                const newCaseId = newIdMap[oldCaseId];
+        for (const oldCaseId of this.oldSmallCaseIds) {
+            if (this.newIdMap[oldCaseId]) {
+                const newCaseId = this.newIdMap[oldCaseId];
 
                 // Get the template for the case
                 const caseTemplate = dbItems[newCaseId];
